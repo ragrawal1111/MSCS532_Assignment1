ac77114c8041b5a78d8a24a298e8430df8f7f4f1
rajivagarwal238-admin <rajivagarwal238@gmail.com>
2026-01-16 20:18:38 -0500

    no message

---
+++ insertion_sort.py
@@ -0,0 +1,13 @@
+#  Insertion Sort algorithm that sorts an array in monotonically decreasing order (largest to smallest, instead of the standard smallest to largest)
+def insertion_sort_desc(arr):
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        # Move elements of arr[0..i-1], that are smaller than key,
+        # to one position ahead of their current position
+        while j >= 0 and key > arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
